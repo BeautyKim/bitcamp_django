cb4e031c9b7ae406d52ba54ad3a3ec3e61086460
BeautyKim <kima2012114@gmail.com>
2022-03-15 18:34:23 +0900

    2022-03-15 타이타닉 1일차

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -5,18 +5,16 @@ from titanic.views import TitanicView
 
 if __name__ == '__main__':
     view = TitanicView()
-    model = TitanicModel(train_fname='train.csv', test_fname='test.csv')
-
     while 1:
-
         menu = input('1.템프릿 2.전처리')
         if menu == '1':
             print(' #### 1. 템플릿 #### ')
-            templates = TitanicTemplates()
+            templates = TitanicTemplates(train_fname='train.csv', test_fname='test.csv')
 
             # view.preprocess('train.csv', 'test.csv')
             break
         elif menu == '2':
             print(' #### 2. 전처리 #### ')
+            model = TitanicModel(train_fname='train.csv', test_fname='test.csv')
         else:
             break
--- titanic/templates.py
+++ titanic/templates.py
@@ -1,11 +1,13 @@
+from icecream import ic
+
 from context.domains import Dataset
 from context.models import Model
+from titanic import TitanicModel
 
 
 class TitanicTemplates(object):
-    def __init__(self):
-        self.model = Model()
+    def __init__(self, train_fname, test_fname):
         self.dataset = Dataset()
+        self.model = Model()
+        self.titanic = TitanicModel(train_fname, test_fname)
 
-        self.a = self.model.new_model()
-        self.b = self.model.save_model()
